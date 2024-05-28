8bc44464af07e16c50193e1dce9679112c02ab4e
brruhyan <134396548+brruhyan@users.noreply.github.com>
2024-05-28 18:19:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import cv2
 from PIL import Image,ImageOps
 import numpy as np
 def import_and_predict(image_data,model):
-    size=(150,150)
+    size=(500,500)
     image=ImageOps.fit(image_data,size, Image.Resampling.LANCZOS)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
